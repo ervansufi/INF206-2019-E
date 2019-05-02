5f7d4c0537e4a6adb54e82103601605b2530c233
Aqil Fiqran <gamingaft@gmail.com>
2019-05-02 10:18:51 +0700

    fix framework

---
+++ resources/views/buat_lelang.blade.php
@@ -0,0 +1,28 @@
+
+@extends('layouts.app')
+
+@section('title')
+    Buat Lelang
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header card-header-info">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    dfsihfks
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
