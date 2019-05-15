3d05c235c7502fea225ad38db9e2493fa816d941
Mulqan <mulqanadi@gmail.com>
2019-05-16 01:08:48 +0700

    Perbaiki view home, bidding, bidding-tambah, bidding-edit, Controller BidController, dan Route untuk bidding

---
+++ app/Http/Controllers/BidController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Users;
+
+class BidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return view('bidding');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($lelang_id)
+    {
+      return view('bidding-tambah');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('bidding-edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="id">
 
 <head>
     <meta charset="utf-8">
@@ -8,110 +8,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Benefish | @yield('title')</title>
-    <!--     Fonts and icons      -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <!-- CSS Files -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('css/paper-kit.css?v=2.2.0')}}" rel="stylesheet" />
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="{{asset('demo/demo.css')}}" rel="stylesheet" />
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md bg-dark fixed-top navbar-color-on-scroll" color-on-scroll="100">
-            <div class="container">
-                <div class="navbar-translate">
-                    <a class="navbar-brand" href="{{ url('/') }}" rel="tooltip" title="Coded by Creative Tim"
-                        data-placement="bottom" target="_blank">
-                        Benefish
-                    </a>
-                    <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navigation" aria-controls="navigation-index" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        @for ($i = 1; $i <= 3; $i++) <span class="navbar-toggler-bar bar1"></span>
-                            @endfor
-                    </button>
-                </div>
-
-                <div class="collapse navbar-collapse justify-content-end bg-aqua" id="navigation">
-                    <ul class="navbar-nav">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/riwayat-bid') }}">{{ __('Bid') }}</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Lelang <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('/buat-lelang') }}">Buat Lelang</a>
-                                    <a class="dropdown-item" href="{{ url('/riwayat-lelang') }}">Riwayat Lelang</a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                    style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <div class="page-header header-filter pt-5 mt-5" style="background-image: url('{{asset('img/nelayan.jpg')}}'); background-size: cover; background-position: top center;">
-            @yield('content')
-        </div>
-        <div class="footer bg-dark text-center">
-            <h6>©
-            <script>
-                document.write(new Date().getFullYear())
-            </script>, made with <i class="fa fa-heart heart"></i> by BENEFISH TEAM</h6>
-        </div>
-
-        <!--   Core JS Files   -->
-        <script src="{{asset('js/core/jquery.min.js')}}" type="text/javascript"></script>
-        <script src="{{asset('js/core/popper.min.js')}}" type="text/javascript"></script>
-        <script src="{{asset('js/core/bootstrap.min.js')}}" type="text/javascript"></script>
-        <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-        <script src="{{asset('js/plugins/bootstrap-switch.js')}}"></script>
-        <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-        <script src="{{asset('js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
-        <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-        <script src="{{asset('js/plugins/moment.min.js')}}"></script>
-        <script src="{{asset('js/plugins/bootstrap-datepicker.js')}}" type="text/javascript"></script>
-        <!-- Control Center for Paper Kit: parallax effects, scripts for the example pages etc -->
-        <script src="{{asset('js/paper-kit.js?v=2.2.0')}}" type="text/javascript"></script>
+    @include('partials.navbar')
+    @yield('content')
+    @include('partials.footer')
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
 </body>
 
 
