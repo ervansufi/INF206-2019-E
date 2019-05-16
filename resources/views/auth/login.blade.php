245190046a064015ee15cdbd65c28df18705055c
Mulqan <mulqanadi@gmail.com>
2019-05-16 22:35:03 +0700

    Perbaiki Halaman Login dan Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,21 @@
 @extends('layouts.app')
 
-@section('title')
-    Login
-@endsection
+@section('title', 'Login')
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-6 ml-auto mr-auto">
+        <div class="col-lg-5 col-md-6">
             <div class="card">
-                <div class="bg-primary card-header card-header-info text-center">{{ __('Login') }}</div>
-
                 <div class="card-body">
+                    <p class="card-title h4">Login</p>
+                    <hr>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <div class="col-md-12">
-                                <input placeholder="Masukkan Email" id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                              <label>Email</label>
+                                <input placeholder="Email" id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -26,11 +23,10 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <div class="col-md-12">
-                                <input placeholder="Masukkan Password" id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="current-password">
-
+                              <label>Password</label>
+                                <input placeholder="Password" id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="current-password">
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -41,7 +37,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-12 text-center">
-                                <button type="submit" class="btn btn-info">
+                                <button type="submit" class="btn btn-primary btn-block">
                                     {{ __('Login') }}
                                 </button>
                             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,68 @@
 @extends('layouts.app')
 
-@section('title')
-    Register
-@endsection
+@section('title', 'Register')
 
 @section('content')
-<div class="container">
+<div class="container mt-4">
     <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-6 ml-auto mr-auto">
+        <div class="col-lg-5 col-md-6">
             <div class="card">
-                <div class="bg-primary text-center card-header card-header-info">{{ __('Register') }}</div>
-
                 <div class="card-body">
+                  <p class="card-title h4">Register</p>
+                  <hr>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <div class="col-md-12 ">
-                                <input placeholder="Masukkan Nama" id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input placeholder="Nama" id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            @if ($errors->has('name'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                <input placeholder="Masukkan No HP" id="no_hp" type="number" class="form-control{{ $errors->has('no_hp') ? ' is-invalid' : '' }}" name="no_hp" value="{{ old('no_hp') }}" required autocomplete="no_hp" autofocus>
+                        <div class="form-group">
+                            <label>No Handphone</label>
+                            <input placeholder="No Handphone" id="no_hp" type="number" class="form-control{{ $errors->has('no_hp') ? ' is-invalid' : '' }}" name="no_hp" value="{{ old('no_hp') }}" required autocomplete="no_hp" autofocus>
 
-                                @if ($errors->has('no_hp'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('no_hp') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('no_hp'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('no_hp') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                <input placeholder="Masukkan Email" id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input placeholder="Email" id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                <input placeholder="Masukkan Password" id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input placeholder="Password" id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                <input placeholder="Masukkan Password Konfirmasi" id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div class="form-group">
+                            <label>Konfirmasi Password</label>
+                            <input placeholder="Konfirmasi Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-12 text-center">
-                                <button type="submit" class="btn btn-info">
+                                <button type="submit" class="btn btn-primary btn-block">
                                     {{ __('Register') }}
                                 </button>
                             </div>
